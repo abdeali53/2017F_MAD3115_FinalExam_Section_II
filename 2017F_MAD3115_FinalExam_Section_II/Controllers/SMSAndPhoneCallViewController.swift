b6afbd83ec4fba4bc112de8a6246283ea9412c18
moxDroid <pritam.world@gmail.com>
2017-11-02 17:47:18 -0400

    Init

---
+++ 2017F_MAD3115_FinalExam_Section_II/Controllers/SMSAndPhoneCallViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  2017F_MAD3115_FinalExam_Section_II
+//
+//  Created by moxDroid on 2017-11-02.
+//  Copyright © 2017 moxDroid. All rights reserved.
+//  Roll No.    :
+//  Name        :
+
+import UIKit
+
+class SMSAndPhoneCallViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 2017F_MAD3115_FinalExam_Section_II/Models/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  2017F_MAD3115_FinalExam_Section_II
+//
+//  Created by moxDroid on 2017-11-02.
+//  Copyright © 2017 moxDroid. All rights reserved.
+//  Roll No.    :
+//  Name        :
+
+import Foundation
+class Location
+{
+    
+}
